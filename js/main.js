256ff6422783f7559f1f792154c48e339333b1a9
germannafol_linux <germannafol@outlook.com>
2022-03-30 17:58:48 -0300

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,56 @@
+const caixaPreco=document.getElementById("preco");
+const caixaQuantidade=document.getElementById("quantidade");
+const caixaSelecao=document.getElementById("caixa-selecao");
+
+
+//3.Seleciona o pai
+const paiPrecoTotal=document.querySelector("#precoTotal");
+
+function criaRespostaTela(mensagem){
+    
+    //1.cria o elemento
+    respostaPrecoTotal = document.createElement("li");
+    //console.log(1);
+
+    //2.add class ao elemento criado
+    respostaPrecoTotal.classList.add("caixaRespostaPrecoTotal")
+    //console.log(2);
+   
+    //4.add tag pai a tag filha
+    paiPrecoTotal.appendChild(respostaPrecoTotal);
+    //console.log(4);
+
+    //5.modificar texto
+    textoPrecoTotal=document.createTextNode(mensagem);
+    //console.log(5);
+
+    //6.coloca o texto no paragrafo
+    respostaPrecoTotal.appendChild(textoPrecoTotal);
+}   
+
+
+let quantidade1;
+let valor1; //em gramas
+const quantidadeTotal=1000; //1 kg= 1000 gramas
+const botao=document.getElementById("botao");
+
+
+
+
+botao.addEventListener('click', regra=>{
+    
+    function regra(quantidade1,valor1){
+    
+        const valorTotal=valor1*quantidadeTotal/quantidade1;
+        const arredondaValortotal=parseFloat(valorTotal.toFixed(2));   
+        //console.log(`O Preço, produto com ${quantidade1} é ${arredondaValortotal} o Kg`);
+        criaRespostaTela(`Produto com ${quantidade1} ${caixaSelecao.value} : R$ ${arredondaValortotal}/Kg`);
+    }
+
+    regra ( caixaQuantidade.value , caixaPreco.value );
+    caixaPreco.focus;
+}
+)
+
+
+
